0a16df0f8b015614150e0cb061ca9efcad892bbd
gmark-binary <g.mark@wustl.edu>
2024-03-18 10:56:03 -0500

    Stuff

--- src/bankapp/Menu.java
+++ src/bankapp/Menu.java
@@ -17,9 +17,16 @@ public class Menu {
 	
 	//Constructor
 	public Menu() {
-		this.in = new Scanner(System.in);
-		this.account = new BankAccount();
-	}
+        this.in = new Scanner(System.in);
+        this.account = createAccountWithUsername();
+    }
+
+    // Prompts the user for a username and creates a new BankAccount
+    private BankAccount createAccountWithUsername() {
+        System.out.println("Enter your username:");
+        String username = in.nextLine();
+        return new BankAccount(username);
+    }
 	
 	//Code that just displays stuff - no tests needed
 	public void displayingOptions() {
