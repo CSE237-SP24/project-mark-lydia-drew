fc8090e5db5257d6c10f99781902b6bc2905d653
DrewButzel <112281163+DrewButzel@users.noreply.github.com>
2024-03-29 19:39:20 -0500

    indent issues

--- src/bankapp/CardMenu.java
+++ src/bankapp/CardMenu.java
@@ -3,20 +3,16 @@ package bankapp;
 import java.util.*;
 
 public class CardMenu {
-
-    private Scanner in;
+	private Scanner in;
 	private BankAccount account;
-
 	//Constructor
 	public CardMenu(BankAccount account) {
 		this.in = new Scanner(System.in);
 		this.account = account;
 	}
-
 	public void cardMenuDisplay(){
 		infiniteCards();
 	}
-	
 	public void infiniteCards() {
 		boolean userQuit = false;
 		System.out.println("Welcome to your card menu.");
@@ -32,7 +28,6 @@ public class CardMenu {
 			userQuit = cardLoop(input);
 		}
 	}
-	
 	private boolean cardLoop(String input) {
 	    switch (input.toLowerCase()) {
 	        case "1":
@@ -49,7 +44,6 @@ public class CardMenu {
 	            return false; // Continue loop
 	    }
 	}
-	    
 	public void addCard() {
 		try{
 			displayOptionForNumber(); // Display prompt for card number
@@ -77,47 +71,44 @@ public class CardMenu {
 	public void displayOptionForNumber() {
 		System.out.println("Please enter your card number: ");
 	}
-
-    public void displayOptionForType() {
-        System.out.println("If this card is a credit card, enter 0. If this card is a debit card, enter 1. ");
-    }
-	
+	public void displayOptionForType() {
+		System.out.println("If this card is a credit card, enter 0. If this card is a debit card, enter 1. ");
+	}
 	//Code that gets user input
 	//No tests needed...for now (probably discuss in future class)
 	public String getValidUserInputNumber() {
 		String number = in.nextLine();
 		return number;
 	}
-
-    public int getValidUserInputType() {
-        int type = in.nextInt();
-        while (type != 0 && type != 1) {
-            System.out.println("Invalid type! Please enter 0 or 1.");
+	
+	public int getValidUserInputType() {
+		int type = in.nextInt();
+		while (type != 0 && type != 1) {
+			System.out.println("Invalid type! Please enter 0 or 1.");
 			System.out.println("What type of card is this?");
 			type = in.nextInt();
-        }
-        return type;
-    }
+		}
+		return type;
+	}
 	
 	//Does work - needs tests
 	public void processingUserSelection(String number, int type) {
 		Card card = new Card();
 		card.setNumber(number);
-        card.setType(type);
+		card.setType(type);
 		System.out.println("Your " + card.getType() + " card (" + card.getNumber() + ") is now linked to your account.");
 	}
-
-	public void displayCardsForAccount() {
-        List<Card> cards = account.getCards();
-        if (cards.isEmpty()) {
-            System.out.println("No cards linked to this account.");
-        } else {
-            System.out.println("Cards linked to account:" + this.account.getUsername());
-            for (Card card : cards) {
-                System.out.println("Card Number: " + card.getNumber() + ", Type: " + card.getType());
-            }
-        }
 	
+	public void displayCardsForAccount() {
+		List<Card> cards = account.getCards();
+		if (cards.isEmpty()) {
+			System.out.println("No cards linked to this account.");
+		}else {
+			System.out.println("Cards linked to account:" + this.account.getUsername());
+			for (Card card : cards) {
+				System.out.println("Card Number: " + card.getNumber() + ", Type: " + card.getType());
+			}
+		}
 	}
 	
 	public int numberOfCards() {
